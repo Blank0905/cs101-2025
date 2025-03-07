2c1abcdcc43a8e8426f3d04f758cc1078a4b2b6b
Blank0905 <109862312+Blank0905@users.noreply.github.com>
2025-03-07 12:43:49 +0800

    Update main1.c

--- qz1/main1.c
+++ qz1/main1.c
@@ -1,1 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int i = 3101;
+
+    int additional = i - 1500;
+    if (additional <= 0) {
+        printf ("70元");
+    }
+        else if (additional <= 100) {
+            printf("80元");    
+        }    
+        else {
+            int count = additional/100;
+            int left = additional%100;
+            if (left > 0){
+                left = 1;
+            }
+            int sum = (left + count)*10 + 70;
+            printf ("%d元\n", sum);
+        }
+    return 0;
+    }
 
